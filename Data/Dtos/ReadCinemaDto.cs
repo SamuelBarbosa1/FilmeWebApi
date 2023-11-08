a02e7d6da59ad0cbe4072ee9e4eba82a9dc0fccd
Samuel <samuelbarbosa2001oliveira@gmail.com>
2023-11-07 22:48:41 -0300

    upload

--- Data/Dtos/ReadCinemaDto.cs
+++ Data/Dtos/ReadCinemaDto.cs
@@ -3,9 +3,8 @@ namespace FilmesApi.Data.Dtos
 {
     public class ReadCinemaDto
     {
-
         public int Id { get; set; }
         public string Nome { get; set; }
-        public ReadEnderecoDto ReadEnderecoDto { get; set; }
+        public ReadEnderecoDto Endereco { get; set; }
     }
 }
